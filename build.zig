ae76bf17625dab6643bb8e9f9bd3280ffe4bc6ec
Bryan VAN PAEMEL <bryan.vanpaemel@gmail.com>
2025-04-12 08:24:02 +0000

    Ref(regex)     Moved tokenizer tests in a separate file, added it the test     step.

--- build.zig
+++ build.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
     const exe_mod = b.createModule(.{
@@ -17,7 +16,7 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(exe);
-    
+
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
@@ -27,13 +26,21 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-
     const exe_unit_tests = b.addTest(.{
         .root_module = exe_mod,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
+    // Add Tokenizer_test.zig to the test step
+    const tokenizer_tests = b.addTest(.{
+        .root_source_file = b.path("src/Tokenizer_test.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&tokenizer_tests.step);
 }
+
